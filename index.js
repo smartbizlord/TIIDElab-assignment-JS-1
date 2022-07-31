635df2dfcb6a1846cabe2f342a2cddab514b97df
Amusa Oladimeji Tongil Byungsa <103539335+smartbizlord@users.noreply.github.com>
2022-07-31 16:34:40 -0700

    Accounted for odd numbers
    the former code would only return a chess board for even numbers but this version works for odd numbers

--- index.js
+++ index.js
@@ -34,16 +34,29 @@
         let i = 1;
         while (i <= width) {
             if (i % 2 > 0) {
-                result += str1.repeat(width / 2);
+                result += str1.repeat(width / 2) + "\n";
             } else {
-                result += str2.repeat(width / 2);
+                result += str2.repeat(width / 2) + "\n";
             }
             i++;
         }
 
         console.log(result);
     } else {
-        return "You have to choose an even width for the chess board!";
+        str1 = " #";
+        str2 = "# ";
+        result = "";
+        let i = 1;
+        while (i <= width) {
+            if (i % 2 > 0) {
+                result += str1.repeat(Math.floor(width / 2)).slice(0,-1) + "\n";
+            } else {
+                result += str2.repeat(Math.floor(width / 2)).slice(0,-1) + "\n";
+            }
+            i++;
+        }
+
+        console.log(result);
     }
  }
 
